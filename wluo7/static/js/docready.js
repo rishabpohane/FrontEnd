4ae89cec1ef320aaba573e45117f1cd61d364f1e
Weijia Luo <wluo7@illinois.edu>
2016-05-02 16:44:15 -0500

    basic functioning demo

--- wluo7/static/js/docready.js
+++ wluo7/static/js/docready.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     
     addSpline('Distance Data', '/data/distance', 'data-container1');
-    addMeter('Gyroscope Data', '/data/gyroscope', 'data-container2');
-    //addSpline('Live Accelerometer Data', '/data/accelerometer', 'data-container2');
-    //addGauge('Live Gyroscope Data', '/data/gyroscope', 'data-container3');
+    addSpline('Gyroscope Data', '/data/gyroscope', 'data-container2');
+    addSpline('Accelerometer Data', '/data/accelerometer', 'data-container3');
+    addSpline('Temperature Data', '/data/temperature', 'data-container4');
     //addGauge('Live Gyroscope Data', '/data/gyroscope', 'data-container4');
     //addGauge('Live Gyroscope Data', '/data/gyroscope', 'data-container5');
     //addSpline('Live Accelerometer Data', '/data/accelerometer', 'data-container6');
